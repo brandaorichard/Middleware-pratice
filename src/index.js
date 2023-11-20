78828ce5af5fc8ca83eec3becf835d7491403798
brandaorichard <rih.brandao@gmail.com>
2023-11-20 10:23:35 -0300

    Criacao e uso do MIDDLEWARE de validacao

--- src/index.js
+++ src/index.js
@@ -11,21 +11,30 @@ const teams = [
 
 // no exemplo abaixo, seguimos o padrao de separacao funcao e endpoint
 
-// funcao buscar times
+// funcao getAllTeams
 const getAllTeams = (req, res) => {
   return res.status(200).json(teams)
 }
 // na sequencia chamamos a funcao como parametro do nosso endpoint get(detalhe importante: chamamos somente a variavel sem a funcao () de fato.)
 app.get('/teams', getAllTeams)
 
-// funcao criar time
+// MIDDLEWARE de validacao para o createTeam, chamamos o diretamente no parametro do endpoint
+const validateName = (req, res, next) => {
+  const { name } = req.body
+  if(!name || typeof name != 'string') {
+    return res.status(400).json({ message: 'nome invalido' })
+  }
+  next()
+}
+
+// funcao createTeam
 const createTeam = (req, res) => {
   const { name } = req.body
   teams.push(name)
   return res.status(201).end(); // uso do end() para indiciar q nao queremos enviar nenhum corpo como resposta
 }
 // endpoint post
-app.post('/teams', createTeam) // chamada da funcao como parametro
+app.post('/teams', validateName, createTeam) // com a seguinte estrutura quando for feito uma requisicao para o endpoint em questao, primeiro ele bate no middleware de validacao e na sequencia CASO FOR VALIDADO ira dar continuidade e bater na proxima funcao q no caso eh a createTeam
 
 app.listen(3000, () => {
   console.log('Xablau!!!');
