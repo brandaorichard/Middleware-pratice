91ac772b84e72e0f8777ebf7a1bb80603ab6ee82
brandaorichard <rih.brandao@gmail.com>
2023-11-20 09:39:44 -0300

    Separacao funcao x endpoint

--- src/index.js
+++ src/index.js
@@ -8,9 +8,23 @@ const teams = [
   'Alecrim'
 ]
 
-app.get('/teams', (req, res) => {
+// no exemplo abaixo, seguimos o padrao de separacao funcao e endpoint
+
+// funcao buscar times
+const getAllTeams = (req, res) => {
   return res.status(200).json(teams)
-})
+}
+// na sequencia chamamos a funcao como parametro do nosso endpoint get(detalhe importante: chamamos somente a variavel sem a funcao () de fato.)
+app.get('/teams', getAllTeams)
+
+// funcao criar time
+const createTeam = (req, res) => {
+  const { name } = req.body
+  teams.push(name)
+  return res.status(201).end(); // uso do end() para indiciar q nao queremos enviar nenhum corpo como resposta
+}
+// endpoint post
+app.post('/teams', createTeam) // chamada da funcao como parametro
 
 app.listen(3000, () => {
   console.log('Xablau!!!');
